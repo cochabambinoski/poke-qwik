77d471e9f59276fae138c1a8ef8091d4471665d3
Feber <cochabambinoski@gmail.com>
2025-09-07 22:37:56 -0400

    changing buttons fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,30 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const pokemonId = useSignal(1);
+
   return (
     <>
-      <span class="text-5xl">Hola Mundo</span>
+      <span class="text-2xl">Buscador simple</span>
+
+      <span class="text-9xl">{pokemonId}</span>
+
+      {/* TODO: crear imagen */}
+      <div class="mt-4">
+        <button class="btn-primary mr-2">Anterior</button>
+        <button class="btn-primary">Siguiente</button>
+      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Poke Qwik",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Poke Qwik",
     },
   ],
 };
